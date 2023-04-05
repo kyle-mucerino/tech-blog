ed853ce6716848f3ea21a9271169ecae80e88bfa
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-05 13:50:40 -0700

    models, controllers, and js folders added

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = express();
 const PORT = process.envPORT || 3001;
 
 const handlebars = require('express-handlebars');
-app.set('view engine', 'handlebars');
-app.engine('handlebars', handlebars({ layoutsDir: __dirname + '/Views/layouts', }));
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({ layoutsDir: __dirname + '/Views/layouts', }));
+extname: 'hbs';
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('main', {layout : 'index'}));
+app.get('/', (req, res) => res.render('main.hbs', {layout : 'index.hbs'}));
 
 app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
