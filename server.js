01e3fac722c01ac3d2af3a67f863dda1cfefe58f
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-05 11:37:59 -0700

    handlebars install

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.envPORT || 3001;
 
+const handlebars = require('express-handlebars');
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({ layoutsDir: __dirname + '/Views/layouts', }));
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello !'));
+app.get('/', (req, res) => res.render('main', {layout : 'index'}));
 
 app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
