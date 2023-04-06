a1d1ff4f04ff5f1a9200d128f6cd99e66f7c0f87
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-06 10:40:58 -0700

    server.js updated

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ const path = require('path');
 const app = express();
 const PORT = process.envPORT || 3001;
 
-const sequelize = require('./')
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const handlebars = require('express-handlebars');
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({ layoutsDir: __dirname + '/Views/layouts', }));
 extname: 'hbs';
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 const hbs = exphbs.create({ helpers });
 
 app.get('/', (req, res) => res.render('main.hbs', {layout : 'index.hbs'}));
 
-app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`)
+  sequelize.sync({ force: false })
+});
