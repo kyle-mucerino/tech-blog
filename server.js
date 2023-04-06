eb2991f04f823981baed3f0b24fd3068c146f1c6
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-06 10:47:02 -0700

    express session with cookies and others added

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({ layoutsDir: __dirname + '/Views/layouts', }));
 extname: 'hbs';
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict"
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
