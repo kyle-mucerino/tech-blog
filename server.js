4cee39a5f1bcdcd61581340bc0615e6b1664163a
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-05 18:04:32 -0700

    added express-handlebars

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.envPORT || 3001;
