c1546c6935c2f38814b72fbed617e5ab975db58a
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-13 16:06:15 -0700

    deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const helpers = require("./utils/helpers");
 const path = require("path");
 
 const app = express();
-const PORT = process.envPORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/config");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
